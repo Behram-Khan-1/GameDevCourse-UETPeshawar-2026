e7504de691454d01fbd7431a71c9587bf540864c
Behram Khan <khanbehram66@gmail.com>
2026-02-18 10:22:40 +0500

    Week 3 - Infinite Runner Class 2
    Prefabs, Obstacles, Coins, Left and Right Movement, Trigger and Collision Functions.

--- Lab/Week 2/InfiniteRunner Project/Assets/Movement.cs
+++ Lab/Week 2/InfiniteRunner Project/Assets/Movement.cs
@@ -1,8 +1,11 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 0.1f;
+    public float forwardSpeed = 0.1f;
+    public float sideSpeed = 0.1f;
+
     // public float scaleSpeed = 0.01f;
 
     void Start()
@@ -13,7 +16,16 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * speed);
+        transform.Translate(Vector3.right * forwardSpeed); //Forward movement
+        //(1,0,0) is right, (0,1,0) is up and (0,0,1) is forward
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.forward * sideSpeed); //Left and Right movement
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(-Vector3.forward * sideSpeed); //Left and Right movement
+        }
 
         //  float X =Input.GetAxis("Horizontal");
         //  float Y = Input.GetAxis("Vertical");
@@ -31,5 +43,29 @@ public class Movement : MonoBehaviour
         // // transform.position +=  transform.forward * speed;
                 
         // transform.localScale = new Vector3(scaleSpeed * transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+
+    void OnCollisionEnter(Collision collisionOther)
+    {
+        // Debug.Log(collisionOther.gameObject.name);
+        // if(collisionOther.gameObject.name == "Obstacle")
+        // {
+        //     Debug.Log("Collided with Obstacle");
+        // }
+
+        if(collisionOther.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Collided with Obstacle");
+        }
+      
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Coin"))
+        {
+            Debug.Log("Collided with Coin");
+            Destroy(other.gameObject);
+        }
     }
 }
